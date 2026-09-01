72f31d97ddbc4b95fe339b21051bc58480931da1
tsuesakul <tanaporn.s@deverhood.com>
2026-09-01 20:29:49 +0700

    feat(server): connect mongoDB

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 //route
 const hospitals = require("./routes/hospitals");
-//enc
+//env
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to database
+connectDB();
+
 const app = express();
+
+//Body parser
+app.use(express.json());
+
 app.use("/api/v1/hospitals", hospitals);
 
-// app.get('/', (req,res) => {
-//     //res.send('<h1>hello from express</h1>');
-//     //res.send({name:'Bob'});
-//     //res.send({name:'Bob'});
-//     //res.sendStatus(400);
-//     //res.status(400).json({success:false})
-//     res.status(200).json({sucess:true, data:{id:1}});
-// })
-
 const PORT = process.env.PORT || 5003;
-app.listen(
+const server = app.listen(
   PORT,
   console.log("server running in", process.env.NODE_ENV, "mode on port", PORT),
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server & exit process
+  server.close(() => process.exit(1));
+});
